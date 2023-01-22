323696b014bc5e6f0c000002197392bd1fe858dc
Navin Chandra <navinchandra772@gmail.com>
2023-01-22 18:12:30 +0530

    Added flight and hotel API's

--- flight_data.py
+++ flight_data.py
@@ -1,8 +1,23 @@
 # get flight data
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
 
 def get_flight_data(source, destination, date, budget):
-
+    # date format: YYYY-MM-DD
     # Get flight data from an API
+    url = "https://skyscanner50.p.rapidapi.com/api/v1/searchFlights"
+
+    querystring = {"origin":source,"destination":destination,"date":date,"adults":"1","currency":"USD","countryCode":"US","market":"en-US"}
+
+    headers = {
+        "X-RapidAPI-Key": os.getenv('RAPID_API_KEY'),
+        "X-RapidAPI-Host": "skyscanner50.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
     flight_data = []
-    
+
     return flight_data
--- hotel_data.py
+++ hotel_data.py
@@ -1,8 +1,24 @@
 # get hotel data
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
 
 def get_hotel_data(destination, start_date, end_date, budget):
 
     # Get hotel data from an API
-    hotel_data = []
+    url = "https://hotels4.p.rapidapi.com/locations/v3/search"
+
+    querystring = {"q":destination,"locale":"en_US","langid":"1033","siteid":"300000001"}
+
+    headers = {
+        "X-RapidAPI-Key": os.getenv('RAPID_API_KEY'),
+        "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    #process response here and store hotel names in organized_hotel_data
+    organized_hotel_data = []
     
-    return hotel_data
+    return organized_hotel_data
